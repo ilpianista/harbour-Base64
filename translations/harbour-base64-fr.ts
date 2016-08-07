2b3456d4d7b032bde8d5c46083544d8f8c1cfd6b
Quent-in <pages_quentin@hotmail.com>
2016-08-07 13:09:19 +0200

    French local

---
+++ translations/harbour-base64-fr.ts
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="41"/>
+        <source>Copy to clipboard</source>
+        <translation>Copier vers le presse-papiers</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="49"/>
+        <source>Clear</source>
+        <translation>Effacer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="56"/>
+        <source>Base64 to Text</source>
+        <translation>Base64 vers Texte</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="69"/>
+        <source>Text to Base64</source>
+        <translation>Texte vers Base64</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="75"/>
+        <source>Any text...</source>
+        <translation>Saisir du texte...</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="83"/>
+        <location filename="../qml/pages/MainPage.qml" line="125"/>
+        <source>Encode</source>
+        <translation>Encoder</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="127"/>
+        <source>Decode</source>
+        <translation>Décoder</translation>
+    </message>
+</context>
+</TS>
